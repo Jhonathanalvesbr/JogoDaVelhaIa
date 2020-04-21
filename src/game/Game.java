7f5f808c688be5dfcafce6b0bd3d343fbf259fb5
Jhonathanalvesbr <Jhonathanalves_br@hotmail.com>
2020-04-21 11:50:14 -0300

    Update Game.java

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,9 +1,13 @@
 package game;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.JFrame;
 
-public class Game {
-
+public class Game implements KeyListener, MouseListener {
+    boolean verifica = false;
     public static void main(String[] args) {
         JFrame frame = new JFrame("Jogo da Velha");
         frame.setSize(400, 400);
@@ -16,20 +20,72 @@ public class Game {
         frame.add(game);
         frame.addMouseListener(game);
         frame.addMouseMotionListener(game);
-
+        Game g = new Game();
+        frame.addKeyListener(g);
+        frame.addMouseListener(g);
+        int op = -1;
         while (true) {
             if (game.players == 1) {
                 game.vezIa();
-            } else if (game.players == 0) {
+            } else if (game.players == 0 && op == -1) {
                 game.vezIa();
-            }
-            if (game.opcao() == 1) {
-                while (game.opcao() == 1) {                    
-                    
+                op = game.opcao();
+                if (op == 0) {
+                    op = -1;
                 }
             }
-            
+            if (op != -1) {
+                while(!g.verifica){
+                    
+                }
+                g.verifica = !g.verifica;
+                op = game.opcao();
+                if(op == 0){
+                    op = -1;
+                }
+            }
         }
     }
 
+    @Override
+    public void keyTyped(KeyEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        verifica = !verifica;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        verifica = !verifica;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
